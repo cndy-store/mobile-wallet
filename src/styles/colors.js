dce8dbafd8c74b34fe96fd45fa35b8ad522536b7
Christian Bäuerlein <fabrik42@gmail.com>
2018-03-10 20:07:26 +0100

    Add colors

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+const rawColors = {
+  grey: '#E6E6E6',
+  pinkLight: '#FFB3B3',
+  pink: '#FF7888',
+  greenLight: '#21A68D',
+  green: '#169185'
+};
+
+const colors = {
+  primary: rawColors.pink,
+  primaryHighlight: rawColors.pinkLight,
+  alternative: rawColors.green,
+  alternativeHighlight: rawColors.greenLight
+};
+
+export { colors, rawColors };

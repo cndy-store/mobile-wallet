c99db3d0e025320e3a9661b3af532497e86a2481
Christian Bäuerlein <fabrik42@gmail.com>
2018-03-24 10:04:18 +0100

    Add experimental transaction to SendScreen

--- src/screens/SendScreen.js
+++ src/screens/SendScreen.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { Button, StyleSheet, View, Text } from 'react-native';
+import { sendPayment } from '../lib/stellarAPI';
 
-export default class SendScreen extends React.Component {
+export class SendScreen extends Component {
   static navigationOptions = ({ navigation }) => {
     const { params } = navigation.state;
 
@@ -14,6 +16,16 @@ export default class SendScreen extends React.Component {
     };
   };
 
+  testTransaction() {
+    const amount = '10.00';
+    const receiver = 'GARYCROHQXWIQIRMGFRVFXDX53Y2RYUOYRJPB6OHLHIQ53AMLXM22EZL';
+
+    console.info('starting...');
+    sendPayment({ amount, receiver, keypair: this.props.keypair })
+      .then(result => console.dir(result))
+      .catch(result => console.warn(result));
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -22,7 +34,27 @@ export default class SendScreen extends React.Component {
           title="Go to Receive"
           onPress={() => this.props.navigation.navigate('Receive')}
         />
+
+        <Text>Send Test Transaction</Text>
+        <Button
+          title="Send transaction"
+          onPress={() => this.testTransaction()}
+        />
       </View>
     );
   }
 }
+
+SendScreen.propTypes = {};
+
+const mapStateToProps = state => {
+  return {
+    keypair: state.keypair.keypair
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SendScreen);

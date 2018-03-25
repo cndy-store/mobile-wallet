ef7ee6d6c1f1ff7c67628122600f59d73c86edbd
Christian Bäuerlein <fabrik42@gmail.com>
2018-03-25 12:24:09 +0200

    Better camera mock

--- src/__mocks__/react-native-camera.js
+++ src/__mocks__/react-native-camera.js
@@ -1,2 +1,21 @@
-const Camera = {};
+import React from 'react';
+const constants = {
+  Aspect: {},
+  BarCodeType: {},
+  Type: {},
+  CaptureMode: {},
+  CaptureTarget: {},
+  CaptureQuality: {},
+  Orientation: {},
+  FlashMode: {},
+  TorchMode: {}
+};
+
+class Camera extends React.Component {
+  static constants = constants;
+  render() {
+    return null;
+  }
+}
+
 export default Camera;

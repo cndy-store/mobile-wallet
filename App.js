cbc2fada00a785f6fc05f0ac2828edeef9274637
Christian Bäuerlein <fabrik42@gmail.com>
2018-04-25 09:57:46 +0200

    Add tachyons

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import NativeTachyons from 'react-native-style-tachyons';
+import { StyleSheet } from 'react-native';
 import RootStack from './src/navigation';
 import { Provider } from 'react-redux';
 import configureStore from './src/store/configureStore';
 
 const store = configureStore();
+
+NativeTachyons.build({}, StyleSheet);
 
 export default class App extends React.Component {
   render() {

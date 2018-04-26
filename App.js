1c9f8f20d1fc3a937814dd21c93c3d3547e1b7ff
Christian Bäuerlein <fabrik42@gmail.com>
2018-04-26 16:48:31 +0200

    Add native base root component for toasts

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NativeTachyons from 'react-native-style-tachyons';
 import { StyleSheet } from 'react-native';
+import { Root } from 'native-base';
 import RootStack from './src/navigation';
 import { Provider } from 'react-redux';
 import configureStore from './src/store/configureStore';
@@ -13,7 +14,9 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <RootStack />
+        <Root>
+          <RootStack />
+        </Root>
       </Provider>
     );
   }

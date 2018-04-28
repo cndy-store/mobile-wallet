1edd1617fbffb0a2beb8cb112f62b45446d8ae73
Christian Bäuerlein <fabrik42@gmail.com>
2018-04-28 10:13:19 +0200

    Style receive component

--- src/components/Receive.js
+++ src/components/Receive.js
@@ -1,21 +1,60 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import {
+  Icon,
+  Container,
+  Header,
+  Left,
+  Right,
+  Body,
+  Title,
+  Content,
+  Tab,
+  Card,
+  CardItem,
+  Tabs
+} from 'native-base';
 import { Button, StyleSheet, View, Text } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 import { encodePublicKey } from '../lib/keypairHelpers';
 
 export class ReceiveScreen extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      qrCodeWidth: null
+    };
+
+    this.setQRCodeWidth = this.setQRCodeWidth.bind(this);
+  }
+
+  setQRCodeWidth(event) {
+    var { x, y, width, height } = event.nativeEvent.layout;
+
+    this.setState({ qrCodeWidth: width });
+  }
+
+  renderQRCode() {
+    if (!this.state.qrCodeWidth) return;
+
     const url = encodePublicKey(this.props.keypair.publicKey());
+    return <QRCode size={this.state.qrCodeWidth} value={url} elc="M" />;
+  }
+
+  render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <QRCode value={url} elc="M" />
-        <Text>Receive Screen</Text>
-        <Button
-          title="Go to Send"
-          onPress={() => this.props.navigation.navigate('Send')}
-        />
-      </View>
+      <Content padder>
+        <Card>
+          <CardItem cardBody onLayout={this.setQRCodeWidth}>
+            {this.renderQRCode()}
+          </CardItem>
+        </Card>
+        <Card>
+          <CardItem header>
+            <Text>This is your public key in form of a QR Code</Text>
+          </CardItem>
+        </Card>
+      </Content>
     );
   }
 }

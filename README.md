73741640ffaa73e2cccce9825c161a232900452a
Christian Bäuerlein <fabrik42@gmail.com>
2018-04-28 14:30:18 +0200

    Add bouncing preloader

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Use [React Native Debugger](https://github.com/jhen0409/react-native-debugger) for debugging.
 
 This project was bootstrapped with [Create React Native App](https://github.com/react-community/create-react-native-app) and then ejected.
+
+
+## Credits
+
+Food icons by [Salinee Pimpakun](https://www.shareicon.net/author/salinee-pimpakun).
--- src/components/TransactionSender/TransactionInProgress.js
+++ src/components/TransactionSender/TransactionInProgress.js
@@ -8,20 +8,46 @@ import {
   Text,
   TextInput
 } from 'react-native';
+import BouncingPreloader from 'react-native-bouncing-preloader';
 import Dimensions from 'Dimensions';
+import { styles as s } from 'react-native-style-tachyons';
+
+const icons = [
+  require('../../../assets/img/food/1.png'),
+  require('../../../assets/img/food/2.png'),
+  require('../../../assets/img/food/3.png'),
+  require('../../../assets/img/food/4.png'),
+  require('../../../assets/img/food/5.png'),
+  require('../../../assets/img/food/6.png')
+];
 
 const { width, height } = Dimensions.get('window');
 
 class TransactionInProgress extends Component {
   render() {
     return (
-      <View>
-        <Text>In Progress...</Text>
+      <View style={styles.container}>
+        <BouncingPreloader
+          icons={icons}
+          leftDistance={-100}
+          rightDistance={-150}
+          speed={1000}
+        />
+        <Text style={s.mt5}>Transaction in Progress...</Text>
       </View>
     );
   }
 }
 
 TransactionInProgress.propTypes = {};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: height * 0.8,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 export default TransactionInProgress;

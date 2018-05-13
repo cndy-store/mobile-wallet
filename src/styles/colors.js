d240a8c1d0d2bb0d626d89c4375fa32fa183f3ae
Christian Bäuerlein <fabrik42@gmail.com>
2018-05-13 13:24:08 +0200

    Update color palette

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,20 +1,42 @@
 import { StyleSheet } from 'react-native';
 
 const rawColors = {
-  grey: '#E6E6E6',
-  pinkLight: '#FFB3B3',
-  pink: '#FF7888',
-  greenLight: '#21A68D',
-  green: '#169185'
+  pink1: 'rgba(255,120,136,1)',
+  pink2: 'rgba(255,236,239,1)',
+  pink3: 'rgba(255,168,179,1)',
+  pink4: 'rgba(255, 77, 98,1)',
+  pink5: 'rgba(255, 31, 57,1)',
+
+  orange1: 'rgba(255,175,120,1)',
+  orange2: 'rgba(255,244,236,1)',
+  orange3: 'rgba(255,203,168,1)',
+  orange4: 'rgba(255,149, 77,1)',
+  orange5: 'rgba(255,122, 31,1)',
+
+  turquoise1: 'rgba(120,255,232,1)',
+  turquoise2: 'rgba(236,255,252,1)',
+  turquoise3: 'rgba(168,255,240,1)',
+  turquoise4: 'rgba( 77,255,225,1)',
+  turquoise5: 'rgba( 31,252,215,1)',
+
+  green1: 'rgba(148,255,120,1)',
+  green2: 'rgba(240,255,236,1)',
+  green3: 'rgba(186,255,168,1)',
+  green4: 'rgba(114,255, 77,1)',
+  green5: 'rgba( 77,254, 31,1)',
+
+  purple1: 'rgba(255,120,211,1)',
+  purple2: 'rgba(255,236,249,1)',
+  purple3: 'rgba(255,168,227,1)',
+  purple4: 'rgba(255, 77,197,1)',
+  purple5: 'rgba(253, 31,180,1)',
+
+  grey: '#E6E6E6'
 };
 
 const semantic = {
-  primary: rawColors.pink,
-  primaryHighlight: rawColors.pinkLight,
-  alternative: rawColors.green,
-  alternativeHighlight: rawColors.greenLight,
-  debit: '#FF0000',
-  credit: '#00FF00'
+  debit: rawColors.purple5,
+  credit: rawColors.green5
 };
 
 const color = StyleSheet.create({

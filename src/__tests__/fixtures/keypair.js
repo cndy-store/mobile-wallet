57b14fc9ae0584e71edd341b15d05caa894bceec
Christian Bäuerlein <fabrik42@gmail.com>
2018-05-13 14:04:27 +0200

    Fixes handling of keypair fixtures in specs

--- src/__tests__/fixtures/keypair.js
+++ src/__tests__/fixtures/keypair.js
@@ -1,9 +1,3 @@
-import { StellarSdk } from '../../lib/stellar';
-const { Keypair } = StellarSdk;
-
 const publicKey = 'GCHSUHOKNZQAKTULV5YTMG5CKU2NKE7UUQVN6JT2JY7337CRBIAZ6LXK';
 const secret = 'SCM7J5YNOPHVPHMY32Q6OI7WRKQETCSG7ZP3YIYYJEO7MWYCDTSRZM3C';
-
-const keypair = Keypair.fromSecret(secret);
-
-export { keypair, publicKey, secret };
+export { publicKey, secret };
--- src/components/Receive.test.js
+++ src/components/Receive.test.js
@@ -2,7 +2,10 @@ import React from 'react';
 import QRCode from 'react-native-qrcode-svg';
 import { render } from '../__tests__/renderer';
 import { Receive } from './Receive';
-import { keypair } from '../__tests__/fixtures/keypair';
+import { secret } from '../__tests__/fixtures/keypair';
+import { StellarSdk } from '../lib/stellar';
+
+const keypair = StellarSdk.Keypair.fromSecret(secret);
 
 it('renders correctly', () => {
   const { toJSON } = render(<Receive keypair={keypair} />);

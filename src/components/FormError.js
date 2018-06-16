94b9f79f087f210b8d11963e22acaffdbc799fc0
Christian Bäuerlein <fabrik42@gmail.com>
2018-06-16 20:43:19 +0200

    Make form errors a bit prettier

--- src/components/FormError.js
+++ src/components/FormError.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Icon, Text, View } from 'native-base';
+import { styles as s } from 'react-native-style-tachyons';
 
 class FormError extends React.Component {
   static propTypes = {
@@ -8,8 +10,22 @@ class FormError extends React.Component {
   };
 
   render() {
-    return <Text>{this.props.message}</Text>;
+    return (
+      <View style={styles.container}>
+        <Icon style={s.pl3} name="warning" />
+        <Text style={s.pl1}>{this.props.message}</Text>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  }
+});
 
 export default FormError;

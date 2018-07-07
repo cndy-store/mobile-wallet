40d9328a38878eff0a25b91bd0a62599325e923d
Christian Bäuerlein <fabrik42@gmail.com>
2018-07-07 14:14:39 +0200

    Add lottie

--- android/app/src/main/java/com/cndywallet/MainApplication.java
+++ android/app/src/main/java/com/cndywallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.cndywallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import io.fixd.rctlocale.RCTLocalePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new RCTLocalePackage(),
             new VectorIconsPackage(),
           new RNCameraPackage(),
